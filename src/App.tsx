0a62657ffa493699b3c43600e848c9dc72a474ea
David Jr <david.junior@unesp.br>
2023-06-14 21:40:53 -0300

    feat: board structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Board from "./components/Board";
 
 function App() {
 	return (
-		<div className="h-screen bg-zinc-800">
+		<div className="h-screen bg-zinc-800 flex  justify-center items-center">
 			<Board />
 		</div>
 	);
